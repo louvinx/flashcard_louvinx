e2ccfd921e59af7d9e7b63d278fd9dd155906efc
louvinx PIERRE <louvinxpierre2203@gmail.com>
2025-09-03 20:11:45 -0400

    feat: add walkthrough.gif

--- app/src/main/java/com/flashcard/MainActivity.kt
+++ app/src/main/java/com/flashcard/MainActivity.kt
@@ -2,6 +2,7 @@ package com.flashcard
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -19,11 +20,18 @@ class MainActivity : AppCompatActivity() {
         val textViewQuestion = findViewById<TextView>(R.id.flashcard_question)
         val textViewAnswer = findViewById<TextView>(R.id.flashcard_answer)
 
+        val main = findViewById<RelativeLayout>(R.id.main)
+        val textViewAnswer1 = findViewById<TextView>(R.id.answer_1)
+        val textViewAnswer2 = findViewById<TextView>(R.id.answer_2)
+        val textViewAnswer3 = findViewById<TextView>(R.id.answer_3)
+
+        val imageViewShow = findViewById<ImageView>(R.id.show)
+        val imageViewHide = findViewById<ImageView>(R.id.hide)
+
         textViewQuestion.setOnClickListener {
             textViewQuestion.visibility = View.INVISIBLE
             textViewAnswer.visibility = View.VISIBLE
         }
-
 
 
         textViewAnswer.setOnClickListener {
@@ -31,10 +39,7 @@ class MainActivity : AppCompatActivity() {
             textViewQuestion.visibility = View.VISIBLE
         }
 
-        val main = findViewById<RelativeLayout>(R.id.main)
-        val textViewAnswer1 = findViewById<TextView>(R.id.answer_1)
-        val textViewAnswer2 = findViewById<TextView>(R.id.answer_2)
-        val textViewAnswer3 = findViewById<TextView>(R.id.answer_3)
+
 
         main.setOnClickListener {
             textViewAnswer1.setBackgroundColor(getResources().getColor(R.color.bg_answer, null))
@@ -44,22 +49,41 @@ class MainActivity : AppCompatActivity() {
 
         textViewAnswer1.setOnClickListener {
             textViewAnswer1.setBackgroundColor(getResources().getColor(R.color.red_color, null))
-            textViewAnswer2.setBackgroundColor(getResources().getColor(R.color.red_color, null))
+            textViewAnswer2.setBackgroundColor(getResources().getColor(R.color.bg_answer, null))
             textViewAnswer3.setBackgroundColor(getResources().getColor(R.color.green_color, null))
         }
 
 
         textViewAnswer2.setOnClickListener {
-            textViewAnswer1.setBackgroundColor(getResources().getColor(R.color.red_color, null))
+            textViewAnswer1.setBackgroundColor(getResources().getColor(R.color.bg_answer, null))
             textViewAnswer2.setBackgroundColor(getResources().getColor(R.color.red_color, null))
             textViewAnswer3.setBackgroundColor(getResources().getColor(R.color.green_color, null))
         }
 
 
         textViewAnswer3.setOnClickListener {
-            textViewAnswer1.setBackgroundColor(getResources().getColor(R.color.red_color, null))
-            textViewAnswer2.setBackgroundColor(getResources().getColor(R.color.red_color, null))
+
+            textViewAnswer1.setBackgroundColor(getResources().getColor(R.color.bg_answer, null))
+            textViewAnswer2.setBackgroundColor(getResources().getColor(R.color.bg_answer, null))
             textViewAnswer3.setBackgroundColor(getResources().getColor(R.color.green_color, null))
+        }
+
+
+        imageViewHide.setOnClickListener {
+            textViewAnswer1.visibility = View.INVISIBLE
+            textViewAnswer2.visibility = View.INVISIBLE
+            textViewAnswer3.visibility = View.INVISIBLE
+            imageViewHide.visibility = View.INVISIBLE
+            imageViewShow.visibility = View.VISIBLE
+        }
+
+
+        imageViewShow.setOnClickListener {
+            textViewAnswer1.visibility = View.VISIBLE
+            textViewAnswer2.visibility = View.VISIBLE
+            textViewAnswer3.visibility = View.VISIBLE
+            imageViewHide.visibility = View.VISIBLE
+            imageViewShow.visibility = View.INVISIBLE
         }
 
     }
